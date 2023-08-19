b2633d97024b482dd590e9abd49f7ea7354c4d17
KashSansanwal <78171612+KashSansanwal@users.noreply.github.com>
2023-08-18 18:48:41 -0600

    finished expenditure-analysis

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -9,7 +9,35 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let result = [];
+  let categorySeen = new Set();
+
+  for (let i = 0; i< transactions.length; i++) {
+    let category = transactions[i].category;
+    
+    if (categorySeen.has(category)) {
+      continue;
+    }
+
+    let total = 0;
+    for (let j = 0; j < transactions.length; j++) {
+      if (transactions[j].category === category) {
+        total += transactions[j].price;
+      }
+    }
+
+    result.push({[category]: total});
+    categorySeen.add(category);
+  }
+  console.log(result);
+  return result;
 }
+
+calculateTotalSpentByCategory([
+  { itemName: 'item1', category: 'cat1', price: 100, timestamp: 1 },
+  { itemName: 'item2', category: 'cat2', price: 200, timestamp: 2 },
+  { itemName: 'item3', category: 'cat1', price: 300, timestamp: 3 },
+  { itemName: 'item4', category: 'cat2', price: 400, timestamp: 4 },
+]);
 
 module.exports = calculateTotalSpentByCategory;
